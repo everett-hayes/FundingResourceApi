0ed9f4f000552b4f068bde1d7d71bd7a0d9480ce
everett-hayes <everetty2k@gmail.com>
2021-02-28 18:06:44 -0600

    Adding some helpful comments

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -1,6 +1,9 @@
 class ResourceController < ApplicationController
     include CurrentUserConcern
 
+    # GET request
+    # used by all users to load active resources http://localhost:3000/resource
+    # used by admins to load inactive resources http://localhost:3000/resource?status=admin
     def index
         if params[:status] == "admin"
             if @current_user.is_admin
@@ -15,11 +18,15 @@ class ResourceController < ApplicationController
         end
     end
 
+    # GET request
+    # used by all users to find a single resource http://localhost:3000/resource/1
     def show
         @resource = Resource.find(params[:id])
         render json: @resource, status: 200
     end
 
+    # POST request
+    # used by logged in users to create new resources
     def create
         if @current_user
             @resource = Resource.create(resource_params)
