0e10ad844a86ab74baa12390dd5411029843d497
everett-hayes <everetty2k@gmail.com>
2021-03-04 22:04:25 -0600

    Env handling cookie

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_funding_api", domain: "https://frozen-tor-16945.herokuapp.com" 
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_funding_api", domain: "https://frozen-tor-16945.herokuapp.com" 
+else
+    Rails.application.config.session_store :cookie_store, key: "_funding_api"
+end
