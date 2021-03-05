0f55073a014f6fd3acda510dda6eb86c788b0d03
everett-hayes <everetty2k@gmail.com>
2021-03-05 17:28:33 -0600

    none

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -6,7 +6,7 @@
 #     :tld_length => 2 
 # }
 
-Rails.application.config.session_store :cookie_store, key: "_funding_api", domain: "https://frozen-tor-16945.herokuapp.com", same_site: none, secure: true
+Rails.application.config.session_store :cookie_store, key: "_funding_api", domain: "https://frozen-tor-16945.herokuapp.com", same_site: :none, secure: true
 
 # if Rails.env == "production"
 #     Rails.application.config.session_store :cookie_store, {
