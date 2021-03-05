cc2a233acd294999093afc05d398bb9aaedbac24
everett-hayes <everetty2k@gmail.com>
2021-03-05 17:20:51 -0600

    plz

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,10 +1,17 @@
-if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, {
-         :key => "_funding_api", 
-         :domain => "https://frozen-tor-16945.herokuapp.com",
-         :same_site => :none,
-         :secure => :true 
-    }
-else
-    Rails.application.config.session_store :cookie_store, key: "_funding_api"
-end
+Rails.application.config.session_store :cookie_store, {
+    :key => "_funding_api", 
+    :domain => "https://frozen-tor-16945.herokuapp.com",
+    :same_site => :none,
+    :secure => :true 
+}
+
+# if Rails.env == "production"
+#     Rails.application.config.session_store :cookie_store, {
+#          :key => "_funding_api", 
+#          :domain => "https://frozen-tor-16945.herokuapp.com",
+#          :same_site => :none,
+#          :secure => :true 
+#     }
+# else
+#     Rails.application.config.session_store :cookie_store, key: "_funding_api"
+# end
