ded05170fcb0cb04c4c7b0c926be6eb38dc254d6
Himanshu Bainwala <hbainwala@gmail.com>
2021-03-05 13:38:11 -0600

    Changed session store

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,5 +1,10 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_funding_api", domain: "https://frozen-tor-16945.herokuapp.com" 
+    Rails.application.config.session_store :cookie_store, {
+         :key => "_funding_api", 
+         :domain => "https://frozen-tor-16945.herokuapp.com",
+         :same_site => :none,
+         :secure => :true 
+}
 else
     Rails.application.config.session_store :cookie_store, key: "_funding_api"
 end
