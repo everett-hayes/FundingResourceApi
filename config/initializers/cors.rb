7f2a26c2f3598850d29fc8bda04c427d039ec5a6
everett-hayes <everetty2k@gmail.com>
2021-03-04 22:36:44 -0600

    trying

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 
+    allow do
+        origins "http://localhost:5500"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+
     # give permission to wherever we host our front end right here in the future plz
     # allow do
     #     origins "our front end domain here"
