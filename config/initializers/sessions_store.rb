21ed11e1f2928dba47011973d0b3500ab34ca24f
everett-hayes <everetty2k@gmail.com>
2021-03-05 17:30:09 -0600

    should be the last one

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,20 +1,5 @@
-# Rails.application.config.session_store :cookie_store, {
-#     :key => '_funding_api', 
-#     :domain => 'https://frozen-tor-16945.herokuapp.com',
-#     :same_site => :none,
-#     :secure => :true,
-#     :tld_length => 2 
-# }
-
-Rails.application.config.session_store :cookie_store, key: "_funding_api", domain: "https://frozen-tor-16945.herokuapp.com", same_site: :none, secure: true
-
-# if Rails.env == "production"
-#     Rails.application.config.session_store :cookie_store, {
-#          :key => "_funding_api", 
-#          :domain => "https://frozen-tor-16945.herokuapp.com",
-#          :same_site => :none,
-#          :secure => :true 
-#     }
-# else
-#     Rails.application.config.session_store :cookie_store, key: "_funding_api"
-# end
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_funding_api", domain: "https://frozen-tor-16945.herokuapp.com", same_site: :none, secure: true
+else
+    Rails.application.config.session_store :cookie_store, key: "_funding_api"
+end
