abd2465df65cfa7ca165c20dd64def66bf6a923b
everett-hayes <everetty2k@gmail.com>
2021-03-04 23:39:42 -0600

    lmfao

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_funding_api", domain: "https://frozen-tor-16945.herokuapp.com" 
+Rails.application.config.session_store :cookie_store, key: "_funding_api" #domain: "https://frozen-tor-16945.herokuapp.com" 
