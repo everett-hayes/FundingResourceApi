0b8ba536ed0a29d25acd7f869ab4b5a5bcc9647c
everett-hayes <everetty2k@gmail.com>
2021-03-04 23:27:35 -0600

    plz god

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,5 +1,1 @@
-if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_funding_api", domain: "https://frozen-tor-16945.herokuapp.com" 
-else
-    Rails.application.config.session_store :cookie_store, key: "_funding_api"
-end
+Rails.application.config.session_store :cookie_store, key: "_funding_api", domain: "https://frozen-tor-16945.herokuapp.com" 
