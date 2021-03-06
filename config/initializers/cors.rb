2f69be0c89db5698be21364fb6b0ab962cf2ad5b
everett-hayes <everetty2k@gmail.com>
2021-03-05 18:48:48 -0600

    depression

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,10 +9,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 
-    # give permission to wherever we host our front end right here in the future plz
-    # allow do
-    #     origins "our front end domain here"
-    #     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    # end
+    allow do
+        origins "https://testy1123.herokuapp.com/"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
 
 end
