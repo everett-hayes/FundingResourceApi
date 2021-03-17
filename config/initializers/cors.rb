7e24d6be957f905ca26bf1fcbf1362a351e61ee9
everett-hayes <everetty2k@gmail.com>
2021-03-16 22:03:54 -0500

    trying to fix stuff

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     # Here we tell CORS that local developrs at port 3000 are allowed to communicate with our API
-    # allow do
-    #     origins "http://127.0.0.1:3000"
-    #     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    # end
+    allow do
+        origins "http://127.0.0.1:3000"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+
+    allow do
+        origins "http://localhost:3000"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
 
     # Here we tell CORS that the frontend on heroku is allowed to communicate with our API
     allow do
