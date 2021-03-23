3566174ffdb8bb20b70429d522330ad426404302
everett-hayes <everetty2k@gmail.com>
2021-03-22 21:08:19 -0500

    url change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,7 +13,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     # Here we tell CORS that the frontend on heroku is allowed to communicate with our API
     allow do
-        origins "https://testy1123.herokuapp.com"
+        origins "https://cls-funding-resources.herokuapp.com"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 
